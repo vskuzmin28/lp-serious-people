da40113c96990f2e74fdc5863eeda430f33678e7
vskuzmin28 <vskuzmin28@gmail.com>
2019-06-08 20:26:19 +0300

    init template, added header

---
+++ app/js/scripts.js
@@ -0,0 +1,10 @@
+// slider
+$('.slider').slider({
+    value: 49,
+    min: 0,
+    max: 100,
+    step: 1,
+});
+
+// checkboxes checked
+$('#check1, #check2, #check3, #check4, #check5, #check10, #check12, #check13, #check14, #check15, #radio2').prop('checked', true);
